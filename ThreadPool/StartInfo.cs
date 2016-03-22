86c90f1bb9e27988fbeb97517f0e1bbec065e98e
AlexanderYao <dwy4670@gmail.com>
2016-03-22 11:56:57 +0800

    add StartInfo

---
+++ ThreadPool/StartInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    public class StartInfo
+    {
+        public Int32 MinWorkerThreads { get; set; }
+        public Int32 MaxWorkerThreads { get; set; }
+        public DropEnum DropEnum { get; set; }
+
+        public StartInfo()
+        {
+            MinWorkerThreads = 2;
+            MaxWorkerThreads = 10;
+            DropEnum = DropEnum.DropNewest;
+        }
+    }
+
+    public enum DropEnum
+    {
+        /// <summary>
+        /// when queue is full, drop the new coming
+        /// </summary>
+        DropNewest,
+        /// <summary>
+        /// when queue is full, dequeue the head and drop it
+        /// </summary>
+        DropOldest
+    }
+}
